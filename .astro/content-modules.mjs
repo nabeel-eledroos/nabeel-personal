96ccfabde4569a723ebed54f9c5ea219d171ec08
nabeel-eledroos <nabeel@eledroos.net>
2026-01-05 23:03:07 -0800

    using astro content collections for photography

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,1 +1,5 @@
-export default new Map();
+
+export default new Map([
+["src/pages/photography/posts/paris-london.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fpages%2Fphotography%2Fposts%2Fparis-london.mdx&astroContentModuleFlag=true")],
+["src/pages/photography/posts/summer-2025.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fpages%2Fphotography%2Fposts%2Fsummer-2025.mdx&astroContentModuleFlag=true")]]);
+		
