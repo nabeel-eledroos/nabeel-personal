8198d7f20a0bcc1c6074b9a91ab4960637c918c9
nabeel-eledroos <nabeel@eledroos.net>
2026-05-31 15:59:36 -0700

    photography content fix

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,4 +1,1 @@
-
-export default new Map([
-["src/pages/photography/posts/paris-london.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fpages%2Fphotography%2Fposts%2Fparis-london.mdx&astroContentModuleFlag=true")]]);
-		
+export default new Map();
